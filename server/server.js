8ea6059d0bfed01c5899c88c70df18d373ab39b9
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-11-02 11:29:19 +0500

    connect data base with backend

--- server/server.js
+++ server/server.js
@@ -1,22 +1,37 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
 
+dotenv.config({ path: "./.env" });
+
+const DataBase = process.env.DATABASE;
+const PORT = process.env.PORT;
+
+mongoose.connect(DataBase)
+    .then(() => {
+        console.log("Data Base connnected ");
+    })
+    .catch((err) => {
+        console.log("Not connected Error");
+    });
+
 app.get("/", (req, res) => {
-    res.send("This is Home Page")
+    res.send("This is Home Page");
 });
+
 app.get("/about", (req, res) => {
-    res.send("This is about Page")
+    res.send("This is about Page");
 });
 
 app.get("/services", (req, res) => {
-    res.send("This is services Page")
+    res.send("This is services Page");
 });
 
 app.get("/login", (req, res) => {
-    res.send("This is login Page")
+    res.send("This is login Page");
 });
 
-
-app.listen(3000, () => {
-    console.log("server started at localhost 3000")
-})
+app.listen(PORT, () => {
+    console.log(`Server Start at ${PORT}`);
+});
