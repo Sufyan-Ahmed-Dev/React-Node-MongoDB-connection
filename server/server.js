a048ace6e630f005fa2069403b8694275c934709
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-11-02 10:20:02 +0500

    making basic routes in node

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (res, req) => {
-    res.send("Hello world")
+app.get("/", (req, res) => {
+    res.send("This is Home Page")
 });
+app.get("/about", (req, res) => {
+    res.send("This is about Page")
+});
+
+app.get("/services", (req, res) => {
+    res.send("This is services Page")
+});
+
+app.get("/login", (req, res) => {
+    res.send("This is login Page")
+});
+
 
 app.listen(3000, () => {
     console.log("server started at localhost 3000")
