80932d9b98d8b04a57df9be3e72d0c1402ca5d5f
Sufyan Ahmed <sufiyan033130@gmail.com>
2022-11-03 10:56:44 +0500

    main file clean

--- server/server.js
+++ server/server.js
@@ -1,36 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+
 const app = express();
 
 dotenv.config({ path: "./.env" });
-
-const DataBase = process.env.DATABASE;
 const PORT = process.env.PORT;
 
-mongoose.connect(DataBase)
-    .then(() => {
-        console.log("Data Base connnected ");
-    })
-    .catch((err) => {
-        console.log("Not connected Error");
-    });
+require("./DataBase/DataBase")
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("This is Home Page");
-});
 
-app.get("/about", (req, res) => {
-    res.send("This is about Page");
-});
-
-app.get("/services", (req, res) => {
-    res.send("This is services Page");
-});
-
-app.get("/login", (req, res) => {
-    res.send("This is login Page");
-});
+app.use(require("./Routes/Routes"))
 
 app.listen(PORT, () => {
     console.log(`Server Start at ${PORT}`);
